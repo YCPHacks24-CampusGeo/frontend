b288c98c47c48b28e8d42e451ffe876e293d4e60
UnicycleUnicorn <brand227796@gmail.com>
2024-11-02 22:14:56 -0400

    always set credentials: include

--- scripts/api.js
+++ scripts/api.js
@@ -3,8 +3,9 @@ const apiURL = 'https://api.ycp.campusgeo.com'
 async function ApiRequest(controller, action, method, body = null) {
     let response = null;
     try {
-        var request = {
+        let request = {
             method: method,
+            credentials: 'include'
         }
 
         if (body != null) {
@@ -12,7 +13,8 @@ async function ApiRequest(controller, action, method, body = null) {
             request.headers = {
                 'Content-Type': 'application/json',
             };
-            request.credentials = 'include';
+            //Remove the following line of code
+            //request.credentials = 'include';
         }
 
         response = await fetch(apiURL + '/' + controller + '/' + action, request);
@@ -29,3 +31,5 @@ async function ApiRequest(controller, action, method, body = null) {
 
     return response;
 }
+
+//This does not promise that every request possesses the cookies in the
